56bd593a33a078d49024ab89d1d1fae7c5d22212
mzmel <mzhardy@hotmail.com>
2014-02-17 15:05:52 -0600

    New version

---
+++ ChatSend/JMBMasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  JMBMasterViewController.h
+//  ChatSend
+//
+//  Created by Presentation on 1/13/14.
+//  Copyright (c) 2014 Joe Burgess. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JMBMasterViewController : UITableViewController
+
+@end
---
+++ ChatSend/JMBMasterViewController.m
@@ -0,0 +1,145 @@
+//
+//  JMBMasterViewController.m
+//  ChatSend
+//
+//  Created by Presentation on 1/13/14.
+//  Copyright (c) 2014 Joe Burgess. All rights reserved.
+//
+
+#import "JMBMasterViewController.h"
+#import "JMBViewController.h"
+#import "JMBAppDelegate.h"
+
+@interface JMBMasterViewController () {
+    JMBAppDelegate *myDelegate;
+}
+
+@end
+
+@implementation JMBMasterViewController
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    myDelegate = [UIApplication sharedApplication].delegate;
+    myDelegate.imageTexts = [[NSMutableArray alloc]  init];
+    myDelegate.images = [[NSMutableArray alloc] init];
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+ 
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+-(void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    [self.tableView reloadData]; 
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    // Return the number of rows in the section.
+    return [myDelegate.imageTexts count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    cell.textLabel.text = myDelegate.imageTexts[indexPath.row];
+    
+    return cell;
+}
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    }   
+    else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+{
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+
+#pragma mark - Navigation
+
+// In a story board-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+    
+    JMBViewController *myJMBViewController = [segue destinationViewController];
+    
+    if ([segue.identifier isEqualToString:@"newMessage"]) {
+        myJMBViewController.imageIndex = -1;
+    } else if ([segue.identifier isEqualToString:@"editMessage"]) {
+        NSIndexPath *ip = [self.tableView indexPathForSelectedRow];
+        myJMBViewController.imageIndex = ip.row;
+    }
+   
+    
+    
+}
+
+
+@end
